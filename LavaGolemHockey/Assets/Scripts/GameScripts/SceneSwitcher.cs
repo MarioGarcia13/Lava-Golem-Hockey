e8d84ea0004058e0f8fd0a77654bf752853d16ea
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-11-06 19:51:35 -0800

    Build

---
+++ LavaGolemHockey/Assets/Scripts/GameScripts/SceneSwitcher.cs
@@ -0,0 +1,91 @@
+using JetBrains.Rider.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static SceneSwitcher Instance;
+
+    //images
+    [SerializeField]
+    GameObject LGHStart;
+    [SerializeField]
+    GameObject LGHBackground;
+    [SerializeField]
+    GameObject WinScreenP1;
+    [SerializeField]
+    GameObject WinScreenP2;
+
+    //buttons
+    [SerializeField]
+    GameObject StartButton;
+    [SerializeField]
+    GameObject ModeSelect;
+
+    [Range(0f, 30f)]
+    public float WinScreenTime = 30f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ShowMenu()
+    {
+        StartButton.SetActive(false);
+        LGHStart.SetActive(false);
+        LGHBackground.SetActive(true);
+        ModeSelect.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        LGHBackground.SetActive(false);
+        ModeSelect.SetActive(false);
+    }
+
+    public void GameScene()
+    {
+        HideMenu();
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowWinScreenP1()
+    {
+        StartCoroutine(P1Timer());
+        ShowMenu();
+    }
+
+    public void ShowWinScreenP2()
+    {
+        StartCoroutine(P2Timer());
+        ShowMenu();
+    }
+
+    IEnumerator P1Timer()
+    {
+        WinScreenP1.SetActive(true);
+        yield return new WaitForSeconds(WinScreenTime);
+        WinScreenP1.SetActive(false);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator P2Timer()
+    {
+        WinScreenP2.SetActive(true);
+        yield return new WaitForSeconds(WinScreenTime);
+        WinScreenP2.SetActive(false);   
+        SceneManager.LoadScene(1);
+    }
+}
--- LavaGolemHockey/Assets/Scripts/PlayerScripts/PlayerManager.cs
+++ LavaGolemHockey/Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -24,7 +24,7 @@ public class PlayerManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
