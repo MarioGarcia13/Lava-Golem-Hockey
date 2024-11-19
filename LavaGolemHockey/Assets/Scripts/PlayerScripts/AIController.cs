8519dd00786ebce20dcbd08045eccd942f45fcca
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-11-19 00:40:52 -0800

    AI move and shoot

--- LavaGolemHockey/Assets/Scripts/PlayerScripts/AIController.cs
+++ LavaGolemHockey/Assets/Scripts/PlayerScripts/AIController.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class AIController : MonoBehaviour
 {
+    //AI Variables
+    private bool isShootingInProgress = false;
+    public Transform shootLocation;
+    public Transform netLocation;
+    public Transform[] targetLocations = new Transform[3];
+    private int currentTargetIndex = 0;
+    private GameObject puckToFollow;
+    private float checkInterval = 1f;
+    private float checkTimer = 0f;
+
     //Input Variables
     private InputActionAsset inputAsset;
     private InputActionMap player;
@@ -32,7 +43,7 @@ public class AIController : MonoBehaviour
     [Range(1f, 200f)]
     public float maxSpeed = 10f;
     [Range(1f, 8000f)]
-    public float moveForce = 1000f;
+    public float moveForce = 4000f;
     [Range(1f, 100f)]
     public float rotationSpeed = 10f;
 
@@ -44,32 +55,23 @@ public class AIController : MonoBehaviour
     //private Coroutine passCoroutine;
     private bool canControl = false;
 
-    /*private Vector3 leftPlayerInitialPosition;
-    private Vector3 rightPlayerInitialPosition;
-
-    private Vector3 p2LeftPlayerInitialPosition;
-    private Vector3 p2RightPlayerInitialPosition;*/
-
-
-
     private void Awake()
-    {/*
-        leftPlayerInitialPosition = leftRB.position;
-        rightPlayerInitialPosition = rightRB.position;
-        */
-        /*leftRB = leftPlayer.GetComponent<Rigidbody>();
-        rightRB = rightPlayer.GetComponent<Rigidbody>();*/
-        //inputAsset = this.GetComponent<PlayerInput>().actions;
-        //player = inputAsset.FindActionMap("PlayerControls");
-
+    {
         GameStateManager.Instance.OnGameStateChanged += HandleGameStateChanged;
-
     }
 
     private void Start()
     {
+        targetLocations[0] = GameObject.Find("1Shot").transform;
+        targetLocations[1] = GameObject.Find("2Shot").transform;
+        targetLocations[2] = GameObject.Find("3Shot").transform;
+        netLocation = GameObject.Find("netLocation").transform;
+        shootLocation = GameObject.Find("shootingLocation").transform;
+
         leftRB = leftPlayer.GetComponent<Rigidbody>();
         rightRB = rightPlayer.GetComponent<Rigidbody>();
+        FindPuckToFollow();
+
     }
 
     private void OnDestroy()
@@ -109,64 +111,147 @@ public class AIController : MonoBehaviour
         canControl = (newState == GameStateManager.GameState.Ready);
     }
 
-    /*private void OnEnable()
-    {
-        moveLeft = player.FindAction("LSMove");
-        moveRight = player.FindAction("RSMove");
-        player.Enable();
-
-    }
-    private void OnDisable()
-    {
-        player.Disable();
-    }*/
-
     private void FixedUpdate()
     {
         if (canControl)
         {
-            MovePlayer(leftRB, movementInputLeft);
-            MovePlayer(rightRB, movementInputRight);
+            checkTimer += Time.fixedDeltaTime;
+            if (checkTimer >= checkInterval)
+            {
+                FindPuckToFollow();
+                checkTimer = 0f;
+            }
+            if (puckToFollow != null)
+            {
+                MoveTowardsPuck(leftRB);
+                MoveTowardsPuck(rightRB);
+            }
         }
     }
+
+    private void ChooseNextTarget()
+    {
+        currentTargetIndex = (currentTargetIndex + 1) % targetLocations.Length;
+    }
+
+    private void FindPuckToFollow()
+    {
+        puckToFollow = GameObject.FindGameObjectWithTag("Puck");
+    }
+
     public void UpdatePuckStatus(PlayerCollisions player)
     {
         if (player.gameObject == leftPlayer)
         {
             leftPlayerHasPuck = player.hasPuck;
+            if (leftPlayerHasPuck)
+            {
+                ChooseNextTarget();
+            }
         }
         else if (player.gameObject == rightPlayer)
         {
             rightPlayerHasPuck = player.hasPuck;
+            if (rightPlayerHasPuck)
+            {
+                ChooseNextTarget();
+            }
         }
     }
 
-    private void MovePlayer(Rigidbody playerRigidbody, Vector2 movementInput)
+    private void MoveTowardsPuck(Rigidbody playerRigidbody)
     {
         PlayerCollisions playerCollisions = playerRigidbody.GetComponent<PlayerCollisions>();
-        if (playerCollisions.isStunned)
-            return;
+        if (playerCollisions.isStunned || isShootingInProgress) return;
 
-        // Convert input to 3D vector
-        Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y);
+        Vector3 targetPosition;
+        bool thisPlayerHasPuck = (playerRigidbody == leftRB && leftPlayerHasPuck) || (playerRigidbody == rightRB && rightPlayerHasPuck);
+        bool otherPlayerHasPuck = (playerRigidbody == leftRB && rightPlayerHasPuck) || (playerRigidbody == rightRB && leftPlayerHasPuck);
 
-        // Apply force for movement
-        playerRigidbody.AddForce(movement * moveForce * Time.fixedDeltaTime, ForceMode.Acceleration);
-
-        // Limit velocity to maxSpeed
-        Vector3 horizontalVelocity = Vector3.ProjectOnPlane(playerRigidbody.velocity, Vector3.up);
-        if (horizontalVelocity.magnitude > maxSpeed)
+        if (thisPlayerHasPuck)
         {
-            Vector3 limitedVelocity = horizontalVelocity.normalized * maxSpeed;
-            playerRigidbody.velocity = new Vector3(limitedVelocity.x, playerRigidbody.velocity.y, limitedVelocity.z);
+            // Player with puck moves towards target location
+            if (targetLocations[currentTargetIndex] == null) return;
+            targetPosition = targetLocations[currentTargetIndex].position;
+        }
+        else if (otherPlayerHasPuck)
+        {
+            // Player without puck moves to net location when the other player has the puck
+            targetPosition = netLocation.position;
+        }
+        else
+        {
+            // Neither player has the puck, so chase the puck
+            if (puckToFollow == null) return;
+            targetPosition = puckToFollow.transform.position;
         }
 
-        // Rotate the player
-        if (movement != Vector3.zero)
+        Vector3 directionToTarget = (targetPosition - playerRigidbody.position).normalized;
+        Vector3 movement = new Vector3(directionToTarget.x, 0, directionToTarget.z);
+
+        // Check if the player has reached the target location
+        float distanceToTarget = Vector3.Distance(playerRigidbody.position, targetPosition);
+        float stoppingDistance = 3f; 
+
+        if (distanceToTarget <= stoppingDistance)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(movement);
-            playerRigidbody.rotation = Quaternion.Slerp(playerRigidbody.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            // Player has reached the target
+            playerRigidbody.velocity = Vector3.zero;
+            playerRigidbody.angularVelocity = Vector3.zero;
+
+            // If the player has the puck and has reached the target rotate towards shoot location and then shoot
+            if (thisPlayerHasPuck && shootLocation != null && !isShootingInProgress)
+            {
+                isShootingInProgress = true;
+                StartCoroutine(RotateAndShoot(playerRigidbody));
+            }
         }
+        else
+        {
+            // Apply force for movement
+            playerRigidbody.AddForce(movement * moveForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+
+            // Limit velocity to maxSpeed
+            Vector3 horizontalVelocity = Vector3.ProjectOnPlane(playerRigidbody.velocity, Vector3.up);
+            if (horizontalVelocity.magnitude > maxSpeed)
+            {
+                Vector3 limitedVelocity = horizontalVelocity.normalized * maxSpeed;
+                playerRigidbody.velocity = new Vector3(limitedVelocity.x, playerRigidbody.velocity.y, limitedVelocity.z);
+            }
+
+            // Rotate the player towards the movement direction
+            if (movement != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(movement);
+                playerRigidbody.rotation = Quaternion.Slerp(playerRigidbody.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    private IEnumerator RotateAndShoot(Rigidbody playerRigidbody)
+    {
+        Vector3 directionToShootLocation = (shootLocation.transform.position - playerRigidbody.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToShootLocation);
+
+        // Rotate towards shoot location
+        while (Quaternion.Angle(playerRigidbody.rotation, targetRotation) > 1f)
+        {
+            playerRigidbody.rotation = Quaternion.Slerp(playerRigidbody.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        // Shooting
+        if (playerRigidbody == leftRB)
+        {
+            ShootPuck(leftPlayer, leftPuckPos);
+        }
+        else if (playerRigidbody == rightRB)
+        {
+            ShootPuck(rightPlayer, rightPuckPos);
+        }
+
+        ChooseNextTarget(); 
+        isShootingInProgress = false;
     }
 
     public void CollisionDetected(PlayerCollisions playerCollision)
@@ -174,7 +259,7 @@ public class AIController : MonoBehaviour
         Debug.Log("Collided");
         if (Vector3.Distance(leftPlayer.transform.position, playerCollision.transform.position) < 2f)
         {
-            leftRB.velocity = leftRB.velocity / 2;
+            leftRB.velocity = leftRB.velocity / 10;
             leftPlayerHasPuck = true;
             Debug.Log("left player has puck");
             rightPlayerHasPuck = false;
@@ -182,7 +267,7 @@ public class AIController : MonoBehaviour
         else if (Vector3.Distance(rightPlayer.transform.position, playerCollision.transform.position) < 2f)
         {
             //reduce velocity
-            rightRB.velocity = rightRB.velocity / 2;
+            rightRB.velocity = rightRB.velocity / 10;
             rightPlayerHasPuck = true;
             Debug.Log("right player has puck");
             leftPlayerHasPuck = false;
@@ -274,67 +359,4 @@ public class AIController : MonoBehaviour
         instance.GetComponent<Rigidbody>().AddForce(directionToReceiver * 80, ForceMode.Impulse);
         //passCoroutine = null;
     }
-
-
-    /*public void OnLSMove(InputAction.CallbackContext ctx)
-    {
-        if (canControl)
-        {
-            movementInputLeft = ctx.ReadValue<Vector2>();
-        }
-    }
-
-    public void OnRSMove(InputAction.CallbackContext ctx)
-    {
-        if (canControl)
-        {
-            movementInputRight = ctx.ReadValue<Vector2>();
-        }
-    }
-
-    public void OnLeftPass(InputAction.CallbackContext ctx)
-    {
-        if (canControl && leftPlayerHasPuck && passCoroutine == null)
-        {
-            passCoroutine = StartCoroutine(HandlePass(leftPlayer, rightPlayer, leftPuckPos));
-        }
-    }
-
-    public void OnRightPass(InputAction.CallbackContext ctx)
-    {
-        if (canControl && rightPlayerHasPuck && passCoroutine == null)
-        {
-            passCoroutine = StartCoroutine(HandlePass(rightPlayer, leftPlayer, rightPuckPos));
-        }
-    }
-
-    public void OnLeftShootTackle(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed && canControl)
-        {
-            if (leftPlayerHasPuck)
-            {
-                ShootPuck(leftPlayer, leftPuckPos);
-            }
-            else
-            {
-                TacklePlayer(leftRB, leftPlayer);
-            }
-        }
-    }
-
-    public void OnRightShootTackle(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed && canControl)
-        {
-            if (rightPlayerHasPuck)
-            {
-                ShootPuck(rightPlayer, rightPuckPos);
-            }
-            else
-            {
-                TacklePlayer(rightRB, rightPlayer);
-            }
-        }
-    }*/
 }
