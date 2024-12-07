538fa4379b4f9ffab7f51d6d2b8f7dbb3dc14239
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-12-06 19:40:06 -0800

    pause menu

--- LavaGolemHockey/Assets/Scripts/GameScripts/ControlsUI.cs
+++ LavaGolemHockey/Assets/Scripts/GameScripts/ControlsUI.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.Rendering.Universal;
 
 public class ControlsUI : MonoBehaviour
 {
     public static bool playersConnected = false;
     public static bool player1Joined = false;
     public static bool player2Joined = false;
-    public bool pauseToggled = false;
+    public bool turnOffLoop = false;
+    public bool togglePause = false;
+    public bool startPressed = false;
     public GameObject controlsUI;
     public GameObject player1Controller;
     public GameObject player2Controller;
     public GameObject player1Conected;
     public GameObject player2Conected;
-
+    public CheckScene checkScene;
 
     private void Start()
     {
         controlsUI.SetActive(true);
+
+        if (player2Controller == null)
+        {
+            return;
+        }
     }
-    public void ShowControls()
+    /*public void ShowControls()
     {
         pauseToggled = true;
         controlsUI.SetActive(true);
-    }
+    }*/
 
     private void Update()
     {
@@ -34,21 +46,37 @@ public class ControlsUI : MonoBehaviour
             
         }
 
-        if (player2Joined)
+        if (checkScene.GetCurrentScene() != 2)
         {
-            player2Controller.SetActive(false);
-            player2Conected.SetActive(true);
+            if (player2Joined)
+            {
+                player2Controller.SetActive(false);
+                player2Conected.SetActive(true);
+            }
+        }
+        
+        if (GameStateManager.Instance.CurrentState == GameStateManager.GameState.Ready)
+        {
+            if (!turnOffLoop)
+            {
+                controlsUI.SetActive(false);
+                turnOffLoop = true;
+            }
         }
 
-        if (GameStateManager.Instance.CurrentState == GameStateManager.GameState.Ready && !pauseToggled)
+        if (Input.GetKeyDown(KeyCode.Escape) && GameStateManager.Instance.CurrentState == GameStateManager.GameState.Ready)
         {
-            controlsUI.SetActive(false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape) && playersConnected)
-        {
-            controlsUI.SetActive(false);
-            pauseToggled= false;
+            togglePause = !togglePause;
+            if (togglePause)
+            {
+                controlsUI.SetActive(togglePause);
+                Time.timeScale = 0f;
+            }
+            if (!togglePause)
+            {
+                Time.timeScale = 1f;
+                controlsUI.SetActive(togglePause);
+            }
         }
     }
 }
